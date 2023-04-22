9271a04ca4408be13f4c05129f13bf67014ed99f
Mario Jose Rodriguez Machacon <75395682+mrodriguezm21@users.noreply.github.com>
2023-04-21 23:05:14 -0500

    Update logger.js

--- components/utils/logger.js
+++ components/utils/logger.js
@@ -1,7 +1,5 @@
 function logger(message, name, type) {
-    let res =  new String();
-
-    res = `[${new Date().toLocaleTimeString('es-CO', {hour12: false, hour: '2-digit', minute:'2-digit', second:'2-digit', fractionalSecondDigits: 3})}] [${name}] [${type}] ${message} `;
+    let res = `[${new Date().toLocaleTimeString('es-CO', {hour12: false, hour: '2-digit', minute:'2-digit', second:'2-digit', fractionalSecondDigits: 3})}] [${name}] [${type}] ${message} `;
 
     return res.toUpperCase();
 }
