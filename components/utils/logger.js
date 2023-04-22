7292916358d785729aaae52ff756bfb78baf6e57
Mario <mariojosexd@gmail.com>
2023-04-21 22:59:53 -0500

    Feat: Nuevo logger de errores

---
+++ components/utils/logger.js
@@ -0,0 +1,9 @@
+function logger(message, name, type) {
+    let res =  new String();
+
+    res = `[${new Date().toLocaleTimeString('es-CO', {hour12: false, hour: '2-digit', minute:'2-digit', second:'2-digit', fractionalSecondDigits: 3})}] [${name}] [${type}] ${message} `;
+
+    return res.toUpperCase();
+}
+
+module.exports = logger;
